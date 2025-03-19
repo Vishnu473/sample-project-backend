9365e55b914cfe037a714c30418694d296aedeb3
Vishnu Kishore Tarini <tarinivishnukishore123@gmail.com>
2025-03-19 22:43:25 +0530

    added delete API endpoint

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,6 +1,8 @@
 import { uploadFiles } from "../middlewares/multer.middleware.js";
 import { asyncHandler } from "../services/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import cloudinary from "../utils/cloudinaryConfig.js";
 
 export const uploadSingleFile = asyncHandler((req, res) => {
   if (!req.file) {
@@ -24,8 +26,38 @@ export const uploadMultipleFiles = asyncHandler((req, res) => {
 
   res.json({
     success: true,
-    urls: fileUrls,
-    files: req.files,
+    url: fileUrls,
+    file: req.files,
     message: `${req.fileCategory}s uploaded successfully`,
   });
+});
+
+export const deleteMedia = asyncHandler(async (req, res) => {
+  const { publicIds } = req.body;
+
+  if (!publicIds || !Array.isArray(publicIds) || publicIds.length === 0) {
+    throw new ApiError("400", "Public IDs are required!");
+  }
+
+  const result = await cloudinary.api.delete_resources(publicIds);
+
+  const failedDeletions = Object.entries(result.deleted).filter(
+    ([, status]) => status !== "deleted"
+  );
+
+  if (failedDeletions.length > 0) {
+    return res
+      .status(500)
+      .json(
+        new ApiResponse(
+          500,
+          { failedDeletions },
+          "Some media could not be deleted."
+        )
+      );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Media deleted successfully"));
 });
--- src/routes/upload.Routes.js
+++ src/routes/upload.Routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/token.middleware.js";
-import { uploadMultipleFiles, uploadSingleFile } from "../controllers/upload.controller.js";
+import { deleteMedia, uploadMultipleFiles, uploadSingleFile } from "../controllers/upload.controller.js";
 import { uploadFiles } from "../middlewares/multer.middleware.js";
 
 const router = Router();
@@ -8,5 +8,5 @@ const router = Router();
 //protected-route
 router.post("/single/:type", verifyToken, uploadFiles("single"), uploadSingleFile);
 router.post("/multiple/:type", verifyToken, uploadFiles("multiple"), uploadMultipleFiles);
-
+router.delete("/delete/media",verifyToken,deleteMedia);
 export default router;
